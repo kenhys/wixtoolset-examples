b4d250c60ed0d31308566de0ee8ddfe36815ef8a
Kentaro Hayashi <hayashi@clear-code.com>
2023-07-21 17:47:17 +0900

    Fix argument

--- jscript/bin/set-env.js
+++ jscript/bin/set-env.js
@@ -1,7 +1,7 @@
 
 function main () {
     var projectLocation = Session.Property["FLUENTPROJECTLOCATION"];
-    var newValue = projectLocation.replace(location, "\\", "/")
+    var newValue = projectLocation.replace("\\", "/")
     var systemEnv = WScript.Shell.Environment("SYSTEM");
     systemEnv("FLUENT_PACKAGE_TOPDIR") = newValue;
 }
